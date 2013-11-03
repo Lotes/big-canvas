3931435c356d4e3ca730f8a35e37643434a567f9
Lotes <markhoernchen@gmx.de>
2013-11-03 21:22:11 +0100

    widgets: finish custom check box

--- public/playground/widgets/jquery.ui.checkbox.js
+++ public/playground/widgets/jquery.ui.checkbox.js
@@ -1,39 +1,50 @@
 (function( $, undefined ) {
 
 $.widget( "ui.checkbox", $.ui.mouse, {
-	version: "@VERSION",
-	widgetEventPrefix: "slide",
+  version: "@VERSION",
+  widgetEventPrefix: "slide",
 
-	options: {
+  options: {
     disabled: false,
-		checked: false,
-
-    //style
+    checked: false,
     checkedClass: "ui-widget-checkbox-checked",
-    
-		//callbacks
-		change: null,
-	},
+	change: null
+  },
 
-	_create: function() {
-		this.element
-			.addClass(
+  _create: function() {
+    var self = this;
+    this.element
+      .addClass(
         "ui-widget-checkbox" +
-				" ui-widget" +
-				" ui-widget-content" +
-				" ui-corner-all");
+        " ui-widget" +
+        " ui-widget-content" +
+        " ui-corner-all")
+	  .click(function() {
+		self._setOption("checked", !self.options.checked);
+	  });
     if(this.options.checked)
       this.element.addClass(this.options.checkedClass);
-	},
+	this._setOption("disabled", this.options.disabled);
+  },
   
-	_destroy: function() {
-		this.element
-			.removeClass(
+  _destroy: function() {
+    this.element
+      .removeClass(
         "ui-widget-checkbox" +
-				" ui-widget" +
-				" ui-widget-content" +
-				" ui-corner-all" );
-	}
+        " ui-widget" +
+        " ui-widget-content" +
+        " ui-corner-all" );
+  },
+  
+  _setOption: function( key, value ) {
+    this._super( key, value );
+    if(key === "checked") {
+      this.element.toggleClass(this.options.checkedClass, !!value);  
+	  if(this.options.change)
+		this.options.change(value);
+    } else if(key === "disabled")
+      this.element.toggleClass("ui-state-disabled", !!value);  
+  }
 });
 
 }(jQuery));
